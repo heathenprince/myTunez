78ae241079456303e9483bd7d06c7443dc4caadd
cyan478 <cyan2@stuy.edu>
2016-10-20 11:59:50 -0400

    still testing

--- list.c
+++ list.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+ #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "list.h"
@@ -20,66 +20,9 @@ song_node * insert_front(song_node *n, char title[], char ar[]){
   return new;
 }
 
-/*
-song_node * insert_order(song_node *n, char title[], char ar[]){
-  
-  song_node *new = (song_node *)malloc(sizeof(song_node));
-
-  strcpy(new->name,title); //new's name
-  strcpy(new->artist,ar); //new's artist
-  //new->next = n; 
-
-  //head is empty
-  song_node *head = (song_node *)malloc(sizeof(song_node));
-  song_node *first = (song_node *)malloc(sizeof(song_node));
-  song_node *curr = (song_node *)malloc(sizeof(song_node));
-  song_node *ncurr = (song_node *)malloc(sizeof(song_node));
-
-  first = n;
-  head = n;
-  curr = n; //1 (curr = current)
-  ncurr = n->next; //2 (ncurr = next current)
-
-  while (curr && ncurr){
-    if (strcmp(title, curr->name) < 0){
-      head = new;
-      head->next = curr;
-      return head; 
-    }
-    
-    //compare w 2
-    if (strcmp(title, ncurr->name) > 0){
-      head = new;
-      curr->next = head; 
-      head->next = curr;
-      return head; 
-    }
-    
-    head = ncurr; //head = 0
-    curr = curr->next; //curr = 1
-    ncurr = curr->next; //ncurr = 2
-  }
-
-  if (curr){ //odd # of songs to compare
-    if (strcmp(title, curr->name) < 0){ //insert after where u left off (ncurr)
-      head = new;
-      head->next = curr;
-      return head; 
-    }
-    else{
-      curr->next = new;
-      return new;
-    }
-  }
-
-return first;
-}
-*/
-
 song_node * insert_order(song_node *n, char title[], char ar[]){
 
-  song_node *head = (song_node *)malloc(sizeof(song_node));
-  head = n;
+  song_node *head = n;
 
   song_node *new = (song_node *)malloc(sizeof(song_node));
   strcpy(new->name,title); //new's name
@@ -90,18 +33,23 @@ song_node * insert_order(song_node *n, char title[], char ar[]){
       new->next = n;
       return new; 
     }
-
   else{
+    printf("%s \n", n->name);
     n = n->next;
     while (n){
       //n = n->next; //new current position
-      if (strcmp(title, n->next->name) < 0){ //check next name if greater
+      if (strcmp(title, (n->next)->name) < 0){ //check next name if greater
+	printf("%s \n", n->name);
         n-> next = new;
+	printf("%s \n", n->next->name);
         new->next = n->next;
-        return head; 
+	printf("%s \n", new->next->name);
+	printf("%s \n", n->name);
+        return head;
       }
       n = n->next; //new current position
     }
+    
     n->next = new;
     return head;
 
@@ -156,8 +104,8 @@ int main() {
   n3 -> next = NULL;
 
   strcpy(n1->artist,"h");
-  strcpy(n2->artist,"h");
-  strcpy(n3->artist,"h");
+  strcpy(n2->artist,"c");
+  strcpy(n3->artist,"p");
 
   strcpy(n1->name,"ayy");
   strcpy(n2->name,"bae");
@@ -174,7 +122,8 @@ int main() {
   //head = insert_front(head, "aaaa","h");
   //print_list(head);
   head = insert_order(head, "ccc","h");
-  print_list(head);
+  //print_list(head);
+  
 
 
   return 0;
